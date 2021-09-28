a09c79b4a43b1beee48f33434119cf73aa3e06d6
Fabian <flindner@geophysik.uni-muenchen.de>
2021-09-28 10:54:06 +0200

    Add fringe-samples option to config file.

--- cube2sds.sh
+++ cube2sds.sh
@@ -13,13 +13,16 @@ source <(grep '^export .*=' config.ini)
 echo "=========================================================================="
 echo "============ Converting DATA-CUBE binaries to MSEED files ... ============"
 echo "=========================================================================="
-$path_cbtool/cube2mseed -v --output-dir=$path_output $path_binary
+$path_cbtool/cube2mseed -v \
+	--output-dir=$path_output $path_binary \
+	--fringe-samples=$fringe_samples
 
 # Rename the DATA-CUBE mseed files created through the previous command using the
 # station mapping provided in the config.ini file and save them in the SDS format.
 echo "=========================================================================="
 echo "============ Casting MSEED files into SDS format ...          ============"
 echo "=========================================================================="
+source activate obspy
 python cube_mseed2sds.py
 # remove the *.pri* files
 rm $path_output/c0*.pri*
